33ed6efa0a15ce98edd2778fbdac970f0be97f14
Carlos León <mail@carlosleon.info>
2013-09-07 16:52:50 +0400

    Adding filter feature for listing different types of invitations.

--- assets/js/app/controllers/invitations_list_ctrl.js
+++ assets/js/app/controllers/invitations_list_ctrl.js
@@ -4,5 +4,14 @@
   angular.module('weddinvApp').
     controller('InvitationsListCtrl', ['$scope', 'Invitation', function($scope, Invitation) {
       $scope.invitations = Invitation.all('invitations').getList();
+
+      $scope.filters = [
+        { label : 'All',      value : ''         },
+        { label : 'Pending',  value : 'pending'  },
+        { label : 'Accepted', value : 'accepted' },
+        { label : 'Rejected', value : 'rejected' }
+      ];
+
+      $scope.search = { status : '' };
     }]);
 }());
