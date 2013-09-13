fd0f20d8ea8e4dcc3f89c5973b0038ca15b629c2
Carlos León <mail@carlosleon.info>
2013-09-14 01:00:34 +0400

    Fixing bug that was not allowing users to load invitations without a session.

--- assets/js/app/controllers/main_ctrl.js
+++ assets/js/app/controllers/main_ctrl.js
@@ -2,16 +2,8 @@
   'use strict';
 
   angular.module('weddinvApp').
-    controller('MainCtrl', ['$rootScope', '$scope', '$location', '$timeout', 'Session', function($rootScope, $scope, $location, $timeout, Session) {
-      var sessionReady;
+    controller('MainCtrl', ['$rootScope', '$scope', '$location', function($rootScope, $scope, $location) {
       $scope.loggedIn = false;
-
-      Session.isLoggedIn(function() {
-        sessionReady    = true;
-        $scope.loggedIn = true;
-      }, function() {
-        sessionReady = true;
-      });
 
       $scope.$on('session:login', function() {
         $scope.loggedIn = true;
@@ -21,15 +13,10 @@
         $scope.loggedIn = false;
       });
 
-      var intervalId = window.setInterval(function() {
-        if (sessionReady) {
-          $rootScope.$on('$routeChangeStart', function(e, next, current) {
-            if (next.loginRequired && !$scope.loggedIn) {
-              $location.path('/login');
-            }
-          });
-          clearInterval(intervalId);
+      $rootScope.$on('$routeChangeStart', function(e, next, current) {
+        if (next.loginRequired && !$scope.loggedIn) {
+          $location.path('/login');
         }
-      }, 200);
+      });
     }]);
 }());
