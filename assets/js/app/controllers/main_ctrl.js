f06389bb1488e6fad06f8e81d05ea386db1247ea
Carlos León <mail@carlosleon.info>
2013-09-19 01:06:09 +0400

    Adding TODO to MainCtrl.

--- assets/js/app/controllers/main_ctrl.js
+++ assets/js/app/controllers/main_ctrl.js
@@ -13,6 +13,10 @@
         $scope.loggedIn = false;
       });
 
+      // TODO: Before sending the user right away to the login page,
+      //       check if there isn't already a session created so that
+      //       the already logged in users don't have to re-login while the
+      //       session is still active (what a waste of time!).
       $rootScope.$on('$routeChangeStart', function(e, next, current) {
         if (next.loginRequired && !$scope.loggedIn) {
           $location.path('/login');
